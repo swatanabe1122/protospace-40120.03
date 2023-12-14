5bbd4ae20d0102192209ba32464ad970732cf624
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2023-12-14 15:05:46 +0900

    最終確認中の間違いの訂正
    投稿画面からユーザーの詳細画面になるように訂正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @name = @user.name
     @prototypes = @user.prototypes
 
     #@name = current_user.name
