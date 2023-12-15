2adf45283c56808cc52c5e30768503fa1f26c845
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2023-12-15 16:04:24 +0900

    修正
    ログアウト状態のユーザーであっても、トップページ・プロトタイプ詳細ページ・ユーザー詳細ページ・ユーザー新規登録ページ・ログインページには遷移できるよう修正しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,10 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,except: [:new, :edit,:destroy]
-  before_action :move_to_index, except: [:index, :show]
-
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
     end
       
     #newアクションにインスタンス変数@prototypeを定義し、Prototypeモデルの新規オブジェクトを代入
@@ -25,11 +25,8 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+    @comments = @prototype.comments
     @comment = Comment.new
-   
-    @prototype = Prototype.find(params[:id])
-    @comments = @prototype.comments.includes(:user)
   end
 
  def edit
@@ -47,11 +44,12 @@ class PrototypesController < ApplicationController
   end
   
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
-
 
 
 
@@ -61,9 +59,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept,).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
