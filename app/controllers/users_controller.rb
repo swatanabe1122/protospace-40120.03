27d247ce2a95053584641a1f54080b466817024a
swatanabe1122 <151703642+swatanabe1122@users.noreply.github.com>
2023-12-15 12:45:46 +0900

    ユーザー詳細機能
    ユーザー名が混在していたのと詳細情報修正、部分テンプレートもなかったので、修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @name = @user.name
     @prototypes = @user.prototypes
-
     #@name = current_user.name
     #@tweets = current_user.tweets
   end
